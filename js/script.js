ddc06890af78b6a3fbbf2507d6352ac14dad2c42
Pearly-choong <choongpl@gmail.com>
2019-08-22 15:28:27 +1200

    jquery methods

--- js/script.js
+++ js/script.js
@@ -1,14 +1,56 @@
 console.log('my script is working');  //to test the page is linked
 
-
+$('.morePara').hide();
+$('.lessBtn').hide();
+$('.btn').show();
 
   $(document).ready(function(){
   	$('#menu').click(function(){
-  		$('nav').toggleClass('active'); 		
-  	})
-  
+  		$('nav').toggleClass('active');
+  	});
+
   	$('#close').click(function(){
   		$('nav').hide();
-  	})
+  	});
+
+
+    // ---------  method 1 : show and hide with two button
+
+    // $('.btn').click(function(){
+    //   $('.morePara').show();
+    //   $('.lessBtn').show();
+    //   $(this).hide();
+    // });
+    //
+    // $('.lessBtn').click(function(){
+    //   $('.morePara').hide();
+    //   $(this).show();
+    //   $('.btn').show();
+    //   $('.lessBtn').hide();
+    // });
+
+    // -------- method 2 : toggle
+    // $('.btn').click(function(){
+    //   $('.morePara').toggle();
+    // });
+
+    //  ----- slide up and slide down function
+    // $('.btn').click(function(){
+    //   $('.morePara').slideDown(3000);
+    // });
+
+
+    // --- toggle with animation
+    $('.btn').click(function(){
+      $('.morePara').slideToggle(3000);
+    });
+    $('#variables').click(function(){
+      $('html, body').animate({
+        scrollTop: $('#features').offset().top + 200 } , 3000);
+      });
+
+
+
+
 
 });
